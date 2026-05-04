098aa2f05fd00bcbb70c3e7580559dc9ec2730ce
AdithyaKulal <adithyakulal94@gmail.com>
2026-05-04 23:35:59 +0530

    Authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
 import { Button } from "@/components/ui/button";
+import SignInFormClient from "@/modules/auth/components/sign-in-form-client";
+import UserButton from "@/modules/auth/components/user-button";
 import Image from "next/image";
+import React from "react";
 
-export default async function Home() {
-
+const Page = () => {
   return (
-    <div className="flex justify-center items-center h-screen" >
-      <Button justify-center>Get started</Button>
+    <div className="flex flex-col
+    items-center justify-center h-screen bg-gray-100">
+      <Button>get started</Button>
+      <UserButton />
     </div>
   );
-}
+};
+
+export default Page;
