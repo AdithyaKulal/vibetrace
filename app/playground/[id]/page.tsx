aa2a24ce9e397a77343401b15526c20d8d333a11
Adithya Kulal <adithyakulal94@gmail.com>
2026-05-10 00:45:07 +0530

    file explorer functionalities

--- app/playground/[id]/page.tsx
+++ app/playground/[id]/page.tsx
@@ -2,6 +2,9 @@
 import React from 'react'
 import { useParams } from 'next/navigation';
 import { usePlayground } from '@/modules/playground/hooks/usePlayground';
+import { TooltipProvider } from '@radix-ui/react-tooltip';
+import { SidebarInset, SidebarTrigger } from '@/components/ui/sidebar';
+import { TemplateFileTree } from '@/modules/playground/components/playground-explorer';
 
 
 const MainPlaygroundPage = () => {
@@ -13,11 +16,41 @@ const MainPlaygroundPage = () => {
     console.log("playgrounddata",playgroundData)
 
 
+    const activeFile = "sample.txt"
     
   return (
-      <div>
-          Params:{id}
-    </div>
+    <TooltipProvider>
+      <>
+        <TemplateFileTree 
+          data={templateData!}
+          onFileSelect={() => { }}
+          selectedFile={activeFile}
+          title="File Explorer"
+          onAddFile={() => { }}
+          onAddFolder={() => { }}
+          onDeleteFile={() => { }}
+          onDeleteFolder={() => { }}
+          onRenameFolder={() => { }}
+          />
+        <SidebarInset>
+          <header className='flex h-16 shrink-0 items-center gap-2 border-b px-4'>
+            <SidebarTrigger className='-ml-1'/>
+
+          </header>
+          <div className='flex flex-1 items-center gap-2'>
+            <div className='flex flex-col flex-1'>
+              <h1 className='text-sm font-medium '>
+                {
+                  playgroundData?.title || "Code playground"
+                }
+              </h1>
+            </div>
+         </div>
+
+        </SidebarInset>
+      </>
+
+      </TooltipProvider>
   )
 }
 
