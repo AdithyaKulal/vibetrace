1693832ad94d09639c4d3b67d232a928e8f3d3de
Adithya Kulal <adithyakulal94@gmail.com>
2026-05-10 12:34:39 +0530

    monacco editor logic part 1

--- app/playground/[id]/page.tsx
+++ app/playground/[id]/page.tsx
@@ -1,57 +1,80 @@
 "use client";
-import React from 'react'
-import { useParams } from 'next/navigation';
-import { usePlayground } from '@/modules/playground/hooks/usePlayground';
-import { TooltipProvider } from '@radix-ui/react-tooltip';
-import { SidebarInset, SidebarTrigger } from '@/components/ui/sidebar';
-import { TemplateFileTree } from '@/modules/playground/components/playground-explorer';
 
+import { Separator } from "@/components/ui/separator";
+import React, { useEffect } from "react";
+import { useParams } from "next/navigation";
+import { usePlayground } from "@/modules/playground/hooks/usePlayground";
+import { TooltipProvider } from "@radix-ui/react-tooltip";
+import { SidebarInset, SidebarTrigger } from "@/components/ui/sidebar";
+import { TemplateFileTree } from "@/modules/playground/components/playground-explorer";
+import { useFileExplorer } from "@/modules/playground/hooks/useFileExplorer";
+import type { TemplateFile } from "@/modules/playground/lib/path-to-json";
 
 const MainPlaygroundPage = () => {
-    const {id} = useParams<{ id: string }>();
+  const { id } = useParams<{ id: string }>();
 
-    const { playgroundData, templateData, isLoading, error, saveTemplateData } = usePlayground(id)
-    
-    console.log("template data:", templateData);
-    console.log("playgrounddata",playgroundData)
+  const { playgroundData, templateData, isLoading, error, saveTemplateData } =
+    usePlayground(id);
+   
+  const {
+    setTemplateData,
+    setActiveFileId,
+    setPlaygroundId,
+    setOpenFiles,
+    activeFileId,
+    closeAllFiles,
+    closeFile,
+    openFile,
+    openFiles,
+  } = useFileExplorer();
+
+  useEffect(() => { setPlaygroundId(id) }, [id, setPlaygroundId]);
+
+  useEffect(() => { 
+
+    if (templateData && !openFiles.length) {
+      setTemplateData(templateData);
+    }
 
 
-    const activeFile = "sample.txt"
-    
+  }, [templateData, setTemplateData, openFiles.length])
+  
+  const activeFile = openFiles.find((file) => file.id === activeFileId)
+  const hasUnsavedChanges = openFiles.some((file) => file.hasUnsavedChanges);
+
+  const handleFileSelect = (file: TemplateFile) => {
+    openFile(file)
+  };
+
   return (
     <TooltipProvider>
       <>
-        <TemplateFileTree 
+        <TemplateFileTree
           data={templateData!}
-          onFileSelect={() => { }}
+          onFileSelect={handleFileSelect}
           selectedFile={activeFile}
           title="File Explorer"
-          onAddFile={() => { }}
-          onAddFolder={() => { }}
-          onDeleteFile={() => { }}
-          onDeleteFolder={() => { }}
-          onRenameFolder={() => { }}
-          />
+          onAddFile={() => {}}
+          onAddFolder={() => {}}
+          onDeleteFile={() => {}}
+          onDeleteFolder={() => {}}
+          onRenameFolder={() => {}}
+        />
         <SidebarInset>
-          <header className='flex h-16 shrink-0 items-center gap-2 border-b px-4'>
-            <SidebarTrigger className='-ml-1'/>
-
+          <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+            <SidebarTrigger className="-ml-1" />
           </header>
-          <div className='flex flex-1 items-center gap-2'>
-            <div className='flex flex-col flex-1'>
-              <h1 className='text-sm font-medium '>
-                {
-                  playgroundData?.title || "Code playground"
-                }
+          <div className="flex flex-1 items-center gap-2">
+            <div className="flex flex-col flex-1">
+              <h1 className="text-sm font-medium ">
+                {playgroundData?.title || "Code playground"}
               </h1>
             </div>
-         </div>
-
+          </div>
         </SidebarInset>
       </>
+    </TooltipProvider>
+  );
+};
 
-      </TooltipProvider>
-  )
-}
-
-export default MainPlaygroundPage
+export default MainPlaygroundPage;
